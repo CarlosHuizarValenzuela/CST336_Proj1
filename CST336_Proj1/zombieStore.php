994e14a98acfdb12c3b01c3f0840eae8aeacd71d
Carlos Huizar Valenzuela <choizarvalenzuela@csumb.edu>
2017-03-29 00:02:58 +0000

    Creating the basis for the website

---
+++ CST336_Proj1/zombieStore.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <title>W3.CSS</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" 
+          href="https://www.w3schools.com/w3css/4/w3.css"
+    />
+    <link rel="stylesheet" 
+              href="myCSS.css" 
+              type="text/css" />
+    <style>
+        .mySlides {display:none;}
+    </style>
+    
+    <body>
+
+        <h1>Zombie Survival Store</h1>
+        
+        <div class="w3-display-container mySlides">
+          <img src="imgs/ak47.jpg" style="width:100%">
+          <div class="w3-display-topleft w3-large w3-container w3-padding-16 w3-black">
+            Beautiful Mountains
+          </div>
+        </div>
+        <div class="w3-display-container mySlides">
+          <img src="imgs/doggoEgg.jpg" style="width:100%">
+          <div class="w3-display-topleft w3-large w3-container w3-padding-16 w3-black">
+            Beautiful Mountains
+          </div>
+        </div>
+        <div class="w3-display-container mySlides">
+          <img src="imgs/fishCakes.jpg" style="width:100%">
+          <div class="w3-display-topleft w3-large w3-container w3-padding-16 w3-black">
+            Beautiful Mountains
+          </div>
+        </div>
+
+        <script>
+        var myIndex = 0;
+        carousel();
+        
+        function carousel() {
+            var i;
+            var x = document.getElementsByClassName("mySlides");
+            for (i = 0; i < x.length; i++) {
+               x[i].style.display = "none";  
+            }
+            myIndex++;
+            if (myIndex > x.length) {myIndex = 1}    
+            x[myIndex-1].style.display = "block";  
+            setTimeout(carousel, 2000); // Change image every 2 seconds
+        }
+        </script>
+
+    </body>
+</html>
